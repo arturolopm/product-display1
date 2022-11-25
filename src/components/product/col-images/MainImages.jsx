adc5738523b950cee99e72b40f149b50f306cf21
R2D2 <carturolopezm@gmail.com>
2022-11-24 21:52:40 -0500

    Worked open and close secnd slider on md

--- src/components/product/col-images/MainImages.jsx
+++ src/components/product/col-images/MainImages.jsx
@@ -6,23 +6,28 @@ const MainImages = ({ ARRAY_IMGS, ARRAY_IMG_SMALL }) => {
     const [isOpenModal, setIsOpenModal] = useState(false)
 
     const handleOpenModal = () => {
-        setIsOpenModal(true)
+        window.innerWidth > 767 && setIsOpenModal(true)
     }
 
+    const handleCloseModal = () => {
+        setIsOpenModal(false)
+    }
     return (
         <>
             <SlideProduct 
                 ARRAY_IMGS={ARRAY_IMGS}
                 ARRAY_IMG_SMALL={ARRAY_IMG_SMALL}
                 className="grid md:grid-cols-4 md:gap-4"
-                onClick={handleOpenModal}
+                handleOpenModal={handleOpenModal}
             />
             {isOpenModal && (
 
                 <SlideProduct 
                     ARRAY_IMGS={ARRAY_IMGS}
                     ARRAY_IMG_SMALL={ARRAY_IMG_SMALL}
-                    className="grid md:absolute md:-translate-y-1/2 md:-translate-x-1/2 md:top-1/2 md:left-1/2 md:max-w-xl md:grid-cols-4 md:gap-4 "
+                    isOpenModal={isOpenModal}
+                    className="hidden md:grid md:absolute md:-translate-y-1/2 md:-translate-x-1/2 md:top-1/2 md:left-1/2 md:max-w-md md:grid-cols-4 md:gap-4 "
+                    handleCloseModal={handleCloseModal}
                 />
                 
             )}
--- src/components/product/col-images/SlideProduct.jsx
+++ src/components/product/col-images/SlideProduct.jsx
@@ -2,7 +2,14 @@ import { useState } from "react"
 import PrevIcon from "@/components/icons/PrevIcon"
 import NextIcon from "@/components/icons/NextIcon"
 
-const SlideProduct = ({ARRAY_IMGS = [], ARRAY_IMG_SMALL=[], ...props}) => {
+const SlideProduct = ({
+    ARRAY_IMGS = [],
+     ARRAY_IMG_SMALL=[], 
+     isOpenModal = false,
+     handleCloseModal= null,
+     handleOpenModal= ()=>{}, 
+     ...props
+    }) => {
 
     const [imgIndex, setImgIndex] = useState(0)
 
@@ -14,10 +21,16 @@ const SlideProduct = ({ARRAY_IMGS = [], ARRAY_IMG_SMALL=[], ...props}) => {
         (imgIndex === 0) ? setImgIndex(ARRAY_IMGS.length-1) : setImgIndex(imgIndex-1)
     }
 
+
+
     return (
         <section {...props}>
+        {
+            isOpenModal && (
+                <button onClick={handleCloseModal} className="md:col-span-4 text-right">cerrar</button>
+        )}
             <div className=" relative col-span-4">
-                <img src={ARRAY_IMGS[imgIndex]} alt="" className=" aspect-auto w-full md:rounded-md" />
+                <img src={ARRAY_IMGS[imgIndex]} alt="" className=" aspect-auto w-full md:cursor-pointer md:rounded-md" onClick={handleOpenModal} />
                 <div className="absolute top-1/2 left-0 flex w-full -translate-y-1/2 justify-between px-4">
                     <button className=" grid place-items-center h-10 w-10 rounded-full bg-white " onClick={handleClickPrev}>
                         <PrevIcon />
