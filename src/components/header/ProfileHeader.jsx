8bbf27c228c1e51697031d3cb75f666468bdf19b
R2D2 <carturolopezm@gmail.com>
2022-12-04 22:37:20 -0500

    fixed stiles for profile display

--- src/components/header/ProfileHeader.jsx
+++ src/components/header/ProfileHeader.jsx
@@ -7,7 +7,7 @@ const ProfileHeader = () => {
         <section className=" absolute top-[125%] right-0 z-10 w-3/5 md:top-full md:left-full md:max-w-md md:-translate-x-full">
             <div className=" mx-4 rounded-md bg-white shadow-md">
 
-                <h4 className=" px-6 py-2 text-lg font-bold text-dark-grayish-blue">
+                <h4 className="  px-6 py-2 text-lg font-bold text-dark-grayish-blue transition-all duration-200 hover:text-orange-primary">
                     Hello
                 </h4>
                 <hr />
@@ -15,11 +15,15 @@ const ProfileHeader = () => {
                      
              
                 <div className=' px-6 pb-1'>
-                <div className=" flex flex-col text-dark-grayish-blue">
-                <a href="#">History purchase</a>
-                <hr />
-                <a href="#">Edit profile</a>
-                <hr />
+                <div className="  flex flex-col text-dark-grayish-blue">
+                <a href="#"className=" relative group hover:text-orange-primary" >History purchase
+                <span className="absolute bottom-0 block w-full h-[1px] scale-x-0 transition-all duration-200 group-hover:bg-orange-primary group-hover:scale-x-100"></span>  
+                </a>
+                <a href="#" className="relative group hover:text-orange-primary">Edit profile
+                <span className=" mb-1 absolute bottom-0 block w-full h-[1px] scale-x-0 transition-all duration-200 group-hover:bg-orange-primary group-hover:scale-x-100"></span>  
+
+                </a>
+                
                 <div className=" text-right">
                 <button className=" h-6 w-2/5 rounded-md bg-dark-grayish-blue text-white transition-all hover:bg-very-dark-blue">Log Out</button>
                 </div>
