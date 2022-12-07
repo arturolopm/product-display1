9c974ac55a62d34b943343c83eca7b4fa87af403
R2D2 <carturolopezm@gmail.com>
2022-12-07 00:29:03 -0500

    added click outside closing function for profile, cart and menu

--- src/components/header/IndexHeader.jsx
+++ src/components/header/IndexHeader.jsx
@@ -5,22 +5,15 @@ import logoJade from "@/assets/images/logo.svg";
 
 import MenuIcon from "@/components/icons/MenuIcon";
 
-import CloseIcon from "@/components/icons/CloseIcon";
-import NavLinkHeader from "@/components/header/NavLinkHeader";
-
 import CartHeader from "@/components/header/CartHeader";
-import ProfileMenu from "./ProfileMenu";
+import ProfileMenu from "@/components/header/ProfileMenu";
+import NavLinks from "@/components/header/NavLinks";
 
 const IndexHeader = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
-
   const handleOpenMenu = () => {
     setIsOpenMenu(true);
   };
-  const handleCloseMenu = () => {
-    setIsOpenMenu(false);
-  };
-
   return (
     <div>
       <header className="  container relative z-10 mx-auto flex items-center gap-8 p-4 md:p-0">
@@ -34,21 +27,7 @@ const IndexHeader = () => {
             alt="Logo Jade"
           />
         </Link>
-        <nav
-          className={` mx-auto flex flex-col bg-white font-bold text-green-primary md:static md:mr-auto md:flex md:h-auto md:flex-row md:gap-4  md:p-0  ${
-            isOpenMenu
-              ? "z-9 fixed top-0 left-0 flex h-full w-2/5 flex-col gap-y-[1px]  p-5  md:p-8"
-              : "hidden"
-          }`}
-        >
-          <button className="mb-12 md:hidden" onClick={handleCloseMenu}>
-            <CloseIcon />
-          </button>
-          <NavLinkHeader text="Products" />
-
-          <NavLinkHeader text="Colections" />
-          <NavLinkHeader text="Contact" />
-        </nav>
+        <NavLinks isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu} />
         <div className=" ml-auto flex gap-4">
           <CartHeader />
           <ProfileMenu />
